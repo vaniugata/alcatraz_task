394cb96c5917f74e46363d2f3358553756ce43b0
vaniugata <vaniugata1988@gmail.com>
2019-02-02 19:36:49 +0200

    Fix my main compilation failure.

--- main.cpp
+++ main.cpp
@@ -50,14 +50,15 @@ TEST_CASE("testing the element addition function")
 // int main()
 // {
 
-// 	// int data[] {1, 2, 5, 6, 7, 8, 12, 34, 52};
-// 	// // float data2[] { 12.35, 8.1, 1.1, 3.14, 5.12, 7.23 };
+// 	int data[] {1, 2, 5, 6, 7, 8, 12, 34, 52};
+// 	// float data2[] { 12.35, 8.1, 1.1, 3.14, 5.12, 7.23 };
 
 
-// 	// Data<int> numericData( data, sizeof(data)/sizeof(data[0]) );
-// 	// numericData.AddElement(222);
-// 	// numericData.PrintData();
-//     // std::cout << "median = " << numericData.FindMedian() << std::endl;
+// 	Data<int> numericData( data, sizeof(data)/sizeof(data[0]) );
+// 	numericData.AddElement(222);
+// 	numericData.AddElement(13);
+// 	numericData.PrintData();
+//     std::cout << "median = " << numericData.FindMedian() << std::endl;
 
 // 	return 0;
 // }
